122afa1fa9054aff0f59513e183b46cbb4ee187d
marcos pinardi <exegeses@gmail.com>
2020-12-14 21:45:38 -0300

    clase Conexion

--- agencia/clases/Conexion.php
+++ agencia/clases/Conexion.php
@@ -2,18 +2,25 @@
 
     class Conexion
     {
-        private $link;
+        private static $link;
 
-        public function conectar()
+        private function __construct()
+        {} // impedimos instanciar la clase
+
+        static function conectar()
         {
-            $this->link = new PDO(
-                'mysql:host=localhost;dbname=agencia',
-                'root',
-                'root'
-            );
-            // control de errores
-            $this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            if( !isset( self::$link ) ){
+                self::$link = new PDO(
+                    'mysql:host=localhost;dbname=agencia',
+                    'root',
+                    'root'
+                );
+            }
 
-            return $this->link;
+            // control de errores
+            self::$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+            return self::$link;
         }
+
     }
