35fe55ed83bc001452cf4c099b8d4185ad5c5547
marcos pinardi <exegeses@gmail.com>
2020-12-21 21:58:25 -0300

    método eliminarDestino()

--- agencia/clases/Destino.php
+++ agencia/clases/Destino.php
@@ -139,6 +139,24 @@
             return false;
         }
 
+        public function eliminarDestino()
+        {
+            $destID = $_POST['destID'];
+            $destNombre = $_POST['destNombre'];
+            $link = Conexion::conectar();
+            $sql = "DELETE FROM destinos
+                        WHERE destID = :destID";
+            $stmt = $link->prepare($sql);
+            $stmt->bindParam(':destID', $destID, PDO::PARAM_INT);
+            if( $stmt->execute() ){
+                //registramos atributos
+                $this->setDestID($destID);
+                $this->setDestNombre($destNombre);
+                return $this;
+            }
+            return false;
+        }
+
         ##################################
         ###  getters & setters
         /**
